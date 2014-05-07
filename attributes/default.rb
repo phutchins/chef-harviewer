2f5f77dd47ac31077ef1a9f5b9f4cd3cfc61d5ef
Philip Hutchins <flipture@gmail.com>
2014-05-07 14:58:36 -0400

    updating to newer version of harviewer

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default['harviewer']['version'] = "2.0.16"
+default['harviewer']['download_url'] = "https://github.com/janodvarko/harviewer/raw/master/releases/"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,2 +1,25 @@
 include_recipe 'apache2'
 include_recipe 'apache2::mod_php5'
+
+package "zip" do
+  action :install
+end
+
+source_file_name = "harviewer-#{node['harviewer']['version']}.zip"
+source_url = "#{File.join(node['harviewer']['download_url'], source_file_name)}"
+
+directory "/var/www/html" do
+  owner "www-data"
+  group "www-data"
+  action :create
+end
+
+remote_file "/var/www/html/harviewer.zip" do
+  source source_url
+  notifies :run, "execute[extract_harviewer]", :immediately
+end
+
+execute "extract_harviewer" do
+  command "cd /var/www/html/ && unzip harviewer.zip"
+  action :nothing
+end
